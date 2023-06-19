728817a95d4a8b15e0230e2ff1b7a7d33595b0ea
Josh DeCristi <joshdecristi@Joshs-MacBook-Pro.local>
2023-06-19 01:22:16 -0400

    feat: setup config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,41 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      primary: '#DE442F',
+      background: '#202030',
+      light: '#2F2F46',
+      dark: '#12121C',
     },
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#641ae6',
+          secondary: '#d926a9',
+          accent: '#1fb2a6',
+          neutral: '#2a323c',
+          'base-100': '#1d232a',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+          transparent: 'transparent',
+          white: '#ffffff',
+          primary: '#DE442F',
+          background: '#202030',
+          light: '#2F2F46',
+          dark: '#12121C',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+};
